08d76a667ee01c41dc59fafd2993a300ed3ecab0
sean <1963966513@qq.com>
2021-05-31 18:23:40 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import demoBlock from './components/demo-block.vue'
+import 'highlight.js/styles/color-brewer.css'
+
+createApp(App)
+  .use(router)
+  .use(ElementPlus)
+  .component('demoBlock', demoBlock)
+  .mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,26 @@
+import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+
+const LoadingComponent = {
+  template: `<div v-loading='true' style='min-height: 500px; width: 100%;'></div>`
+};
+const ErrorComponent = {
+  template: `
+    <div style="text-align: center;padding: 100px 0;">Loading error. Please refresh the page and try again</div>`,
+};
+
+const getAsyncComponent = function(func) {
+  return defineAsyncComponent({
+    loader: func,
+    delay: 0,
+    timeout: 30000,
+    errorComponent: ErrorComponent,
+    loadingComponent: LoadingComponent,
+  })
+}
+
+const component = getAsyncComponent(() => import(/* webpackChunkName: "DOCS fr-FR" */'../views/md/card.md'))
 
 const routes = [
   {
@@ -14,6 +35,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/mdtest',
+    name: 'MDTEST',
+    component: component,
   }
 ]
 
